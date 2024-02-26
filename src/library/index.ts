2448a4ca596f6b5766772e7812758cc9bf253457
Christian Smith Mantas <casm202@gmail.com>
2024-02-26 02:14:26 +0100

    WIp: add songs to playlist library.

--- src/library/index.ts
+++ src/library/index.ts
@@ -55,13 +55,118 @@ export const moods: IMood[] = [
                 ]
             },
             {
-                name: "code-fi[]",
+                name: "code-fi",
                 songs: [
                     {
-                        title: "Close to You",
-                        artist: "The Carpenters - Reneé Dominique Cover",
-                        src: '/audio/playlists/old-songs-but-lofi/cant-take-my-eyes-off-you.mp3'
-                    }
+                        title: "Nothing Really matters that much to me anymore",
+                        artist: "Astroblk",
+                        src: '/audio/playlists/code-fi/nothing-really-matters-that-much-to-me-anymore.mp3'
+                    },
+                    {
+                        title: "Sanctuary",
+                        artist: "Aviscerall",
+                        src: "/audio/playlists/code-fi/sanctuary.mp3",
+                    },
+                    {
+                        title: "Explorer",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/explorer.mp3",
+                    },
+                    {
+                        title: "Can't Replace You",
+                        artist: "Knwln Ryan",
+                        src: "/audio/playlists/code-fi/cant-replace-you.mp3",
+                    },
+                    {
+                        title: "How Can You Tell",
+                        artist: "NetNavi",
+                        src: "/audio/playlists/code-fi/how-can-you-tell.mp3",
+                    },
+                    {
+                        title: "Vancouver",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/vancouver.mp3",
+                    },
+                    {
+                        title: "Lullabyes",
+                        artist: "Aviscerall",
+                        src: "/audio/playlists/code-fi/lullabyes.mp3",
+                    },
+                    {
+                        title: "Courage",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/courage.mp3",
+                    },
+                    {
+                        title: "Coffee Shop",
+                        artist: "Aviscerall",
+                        src: "/audio/playlists/code-fi/coffee-shop.mp3",
+                    },
+                    {
+                        title: "Chillin'",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/chillin.mp3",
+                    },
+                    {
+                        title: "Fields",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/fields.mp3",
+                    },
+                    {
+                        title: "Progress",
+                        artist: "Aviscerall",
+                        src: "/audio/playlists/code-fi/progress.mp3",
+                    },
+                    {
+                        title: "Geneva",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/geneva.mp3",
+                    },
+                    {
+                        title: "Jupiter",
+                        artist: "Knwln Ryan",
+                        src: "/audio/playlists/code-fi/jupiter.mp3",
+                    },
+                    {
+                        title: "Ginger Spice",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/ginger-spice.mp3",
+                    },
+                    {
+                        title: "Clockin' Out",
+                        artist: "Aviscerall",
+                        src: "/audio/playlists/code-fi/clockin-out.mp3",
+                    },
+                    {
+                        title: "Waterfalls",
+                        artist: "Knwln Ryan",
+                        src: "/audio/playlists/code-fi/waterfalls.mp3",
+                    },
+                    {
+                        title: "Feelings",
+                        artist: "Aviscerall",
+                        src: "/audio/playlists/code-fi/feelings.mp3",
+                    },
+                    {
+                        title: "Soul in Seoul",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/soul-in-seoul.mp3",
+                    },
+                    {
+                        title: "To a Song",
+                        artist: "NetNavi",
+                        src: "/audio/playlists/code-fi/to-a-song.mp3",
+                    },
+                    {
+                        title: "Sunrise",
+                        artist: "Astroblk",
+                        src: "/audio/playlists/code-fi/sunrise.mp3",
+                    },
+                    {
+                        title: "Onett",
+                        artist: "Aviscerall",
+                        src: "/audio/playlists/code-fi/onett.mp3",
+                    },
                 ]
             }
         ]
