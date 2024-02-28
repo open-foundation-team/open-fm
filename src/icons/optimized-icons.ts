d981681fa0c3c02522686a60f9bb3c3086f0288e
Christian Smith Mantas <casm202@gmail.com>
2024-02-29 00:23:58 +0100

    Update: replace icons through out app.

---
+++ src/icons/optimized-icons.ts
@@ -0,0 +1,59 @@
+/* eslint-disable */
+// This file is auto-generated, please don't modify it directly. If you want to make changes, make them on $PROJECT/scripts/iconsToJson.ts instead
+export const icons = {
+  "backward": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"backward__w-6 backward__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689zm-9.75 0c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977z\"/></svg>",
+    "jsx": "\n    export const Icon_backward = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"backward__w-6 backward__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689zm-9.75 0c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977z\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"backward\" />"
+  },
+  "chevron-down": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"chevron-down__w-6 chevron-down__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"m19.5 8.25-7.5 7.5-7.5-7.5\"/></svg>",
+    "jsx": "\n    export const Icon_chevron-down = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"chevron-down__w-6 chevron-down__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"m19.5 8.25-7.5 7.5-7.5-7.5\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"chevron-down\" />"
+  },
+  "cloud": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"cloud__w-6 cloud__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.5 4.5 0 0 0 2.25 15\"/></svg>",
+    "jsx": "\n    export const Icon_cloud = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"cloud__w-6 cloud__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.5 4.5 0 0 0 2.25 15\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"cloud\" />"
+  },
+  "coffee": {
+    "svg": "<svg fill=\"none\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path fill=\"currentColor\" d=\"M6 2.5a1 1 0 0 0-1 1v2a1 1 0 0 0 2 0v-2a1 1 0 0 0-1-1m7 19a6 6 0 0 0 5.917-5H19a4 4 0 0 0 0-8v-1H1v8a6 6 0 0 0 6 6zM3 9.5v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4v-6zm18 3a2 2 0 0 1-2 2v-4a2 2 0 0 1 2 2m-12-9a1 1 0 1 1 2 0v2a1 1 0 1 1-2 0zm5-1a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1\"/></svg>",
+    "jsx": "\n    export const Icon_coffee = () => (\n      <svg fill=\"none\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path fill=\"currentColor\" d=\"M6 2.5a1 1 0 0 0-1 1v2a1 1 0 0 0 2 0v-2a1 1 0 0 0-1-1m7 19a6 6 0 0 0 5.917-5H19a4 4 0 0 0 0-8v-1H1v8a6 6 0 0 0 6 6zM3 9.5v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4v-6zm18 3a2 2 0 0 1-2 2v-4a2 2 0 0 1 2 2m-12-9a1 1 0 1 1 2 0v2a1 1 0 1 1-2 0zm5-1a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"coffee\" />"
+  },
+  "fire": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"fire__w-6 fire__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.3 8.3 0 0 0 9 9.601a8.98 8.98 0 0 1 3.361-6.867 8.2 8.2 0 0 0 3 2.48Z\"/><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 18a3.75 3.75 0 0 0 .495-7.468 6 6 0 0 0-1.925 3.547 6 6 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18\"/></svg>",
+    "jsx": "\n    export const Icon_fire = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"fire__w-6 fire__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.3 8.3 0 0 0 9 9.601a8.98 8.98 0 0 1 3.361-6.867 8.2 8.2 0 0 0 3 2.48Z\"/><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 18a3.75 3.75 0 0 0 .495-7.468 6 6 0 0 0-1.925 3.547 6 6 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"fire\" />"
+  },
+  "forward": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"forward__w-6 forward__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811zm9.75 0c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977z\"/></svg>",
+    "jsx": "\n    export const Icon_forward = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"forward__w-6 forward__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811zm9.75 0c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977z\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"forward\" />"
+  },
+  "loop": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"loop__w-6 loop__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M19.5 12q0-1.848-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 49 49 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7q-.025.33-.046.662M19.5 12l3-3m-3 3-3-3m-12 3q0 1.848.138 3.662a4.006 4.006 0 0 0 3.7 3.7 49 49 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7q.025-.33.046-.662M4.5 12l3 3m-3-3-3 3\"/></svg>",
+    "jsx": "\n    export const Icon_loop = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"loop__w-6 loop__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M19.5 12q0-1.848-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 49 49 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7q-.025.33-.046.662M19.5 12l3-3m-3 3-3-3m-12 3q0 1.848.138 3.662a4.006 4.006 0 0 0 3.7 3.7 49 49 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7q.025-.33.046-.662M4.5 12l3 3m-3-3-3 3\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"loop\" />"
+  },
+  "pause": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"pause__w-6 pause__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M15.75 5.25v13.5m-7.5-13.5v13.5\"/></svg>",
+    "jsx": "\n    export const Icon_pause = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"pause__w-6 pause__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M15.75 5.25v13.5m-7.5-13.5v13.5\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"pause\" />"
+  },
+  "play": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"play__w-6 play__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986z\"/></svg>",
+    "jsx": "\n    export const Icon_play = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"play__w-6 play__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986z\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"play\" />"
+  },
+  "settings-horizontal": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"settings-horizontal__w-6 settings-horizontal__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75\"/></svg>",
+    "jsx": "\n    export const Icon_settings-horizontal = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"settings-horizontal__w-6 settings-horizontal__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"settings-horizontal\" />"
+  },
+  "share": {
+    "svg": "<svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"share__w-6 share__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186m0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185\"/></svg>",
+    "jsx": "\n    export const Icon_share = () => (\n      <svg fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.5\" class=\"share__w-6 share__h-6\" viewBox=\"0 0 24 24\" width=\"1em\" height=\"1em\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186m0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185\"/></svg>\n    );\n  ",
+    "component": "<Icon name=\"share\" />"
+  }
+};
